52e228c0817cd35ac993dd1ab943032db9b68925
andrepilco <andrepilcoch@gmail.com>
2024-11-07 17:40:56 -0500

    angular

--- ml-project/crypto-app/app.py
+++ ml-project/crypto-app/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import json
 from services.model import Model
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/predict", methods=["POST"])
 def predict():
